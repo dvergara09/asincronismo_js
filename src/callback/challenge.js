6321d7b1a0db1152b72ea6cf8c09ba4b54eeb48e
Dnaiel Vergara <danielvsuarez01@gmail.com>
2020-04-20 17:32:34 -0500

    challenge

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,9 +1,10 @@
 let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+let API = "https://rickandmortyapi.com/api/character/";
 
 function fetchData(url_api, callback) {
   let xhttp = new XMLHttpRequest();
   xhttp.open("GET", url_api, true);
-  xhttp.onreadystatechange = function(event) {
+  xhttp.onreadystatechange = function (event) {
     /* readystate, estado que todo está bien en http */
     if (xhttp.readyState === 4) {
       /* status, que la informacion no tuvo ningun error */
@@ -17,3 +18,16 @@ function fetchData(url_api, callback) {
   };
   xhttp.send();
 }
+
+fetchData(API, function (error1, data1) {
+  if (error1) return console.error(error1);
+  fetchData(API + data1.results[0].id, function (error2, data2) {
+    if (error2) return console.error(error2);
+    fetchData(API + data2.origin.url, function (error3, data3) {
+      if (error3) return console.error(error3);
+      console.log(data1.info.count);
+      console.log(data2.name);
+      console.log(data3.dimension);
+    });
+  });
+});
