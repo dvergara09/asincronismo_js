86b6f7874f2a83529b423891cf1359177262906e
Dnaiel Vergara <danielvsuarez01@gmail.com>
2020-04-21 06:41:10 -0500

    empezando con promesas

---
+++ src/promise/index.js
@@ -0,0 +1,38 @@
+const somethingWillHappen = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve("Hey");
+    } else {
+      reject("Whooops");
+    }
+  });
+};
+
+somethingWillHappen()
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+
+const somethingWillHappen2 = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      setTimeout(() => {
+        resolve("hey");
+      }, 3000);
+    } else {
+      const error = new Error("Whoops");
+      reject(error);
+    }
+  });
+};
+
+somethingWillHappen2()
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+  .then((response) => {
+    console.log("array of results ", response);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
